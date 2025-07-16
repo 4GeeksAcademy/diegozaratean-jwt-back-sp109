a80e99dac1ed3d5cca2088953066a096bc12e276
diego <diegozaratean@gmail.com>
2025-07-16 18:32:40 +0000

    finish back y front

--- src/app.py
+++ src/app.py
@@ -9,6 +9,13 @@ from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User
+
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+
+from sqlalchemy import select
 #from models import Person
 
 app = Flask(__name__)
@@ -26,6 +33,19 @@ db.init_app(app)
 CORS(app)
 setup_admin(app)
 
+
+# Setup the Flask-JWT-Extended extension
+app.config["JWT_SECRET_KEY"] = "super-secret3t lo mas seguro que ustedesw pueen tener"  # Change this!
+jwt = JWTManager(app)
+
+# a ===> asdfadsfasdfasdfasdfadsf
+# asuper-secret =====> adfsgadgadsfadsfadfsfasdf
+# hugo@mail.com ===> 3jUMi4
+# hugo@mail.com ===> V-FnfI
+# casa ====> casaasdfadsfasdfasdfasdfotra
+# perro =====> 1231231233ui1gh2iu3h12iu3h12iu3hyiu123hy
+# xxxxxx ====> igualdefacilotra
+
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -41,6 +61,45 @@ def handle_hello():
 
     response_body = {
         "msg": "Hello, this is your GET /user response "
+    }
+
+    return jsonify(response_body), 200
+
+@app.route("/login", methods=["POST"])
+def login():
+    email = request.json.get("email", None)
+    print(email)
+    password = request.json.get("password", None)
+    # user = User.query.filter_by(email=email).first()
+    user = db.session.execute(select(User).where(User.email == email)).scalar_one_or_none()
+    if user is None:
+        return jsonify({"msg": "Bad username or password"}), 401
+    print(user)
+    if password != user.password:
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    access_token = create_access_token(identity=email)
+    return jsonify(access_token=access_token)
+
+@app.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
+
+
+@app.route('/secret', methods=['GET'])
+@jwt_required()
+def top_secret():
+
+    response_body = {
+        "msg": "estos son los mayores secretos",
+        "secretos":[
+            "los pajaros no existen son drones",
+            "avril lavigne es un clon/hermana",
+            "los reptilainos estan entre nosotros "
+        ]
     }
 
     return jsonify(response_body), 200
